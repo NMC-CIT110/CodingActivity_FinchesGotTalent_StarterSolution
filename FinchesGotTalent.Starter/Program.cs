6ab6b851e9fc109453f5a7c4093a340d02008250
John Velis <john@johnvelis.com>
2016-08-17 16:02:47 -0400

    Initial Commit

---
+++ FinchesGotTalent.Starter/Program.cs
@@ -0,0 +1,51 @@
+﻿using FinchAPI;
+using System;
+
+namespace CodingActivity_FinchesGotTalent_StarterSolution
+{
+    class Program
+    {
+        /// <summary>
+        /// Finches Got Talent Application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // **************************************************
+            //
+            // Title: Finches Got Talent
+            // Description: (add your description)
+            // Application Type: Console
+            // Author: (add your name)
+            // Dated Created: (current date)
+            // Last Modified:
+            //
+            // **************************************************
+
+            //
+            // create (instantiate) a new finch object
+            //
+            Finch myFinch;
+            myFinch = new Finch();
+
+            //
+            // connect to the finch robot using the finch connect method
+            //
+            myFinch.connect();
+
+            //
+            // begin application code here
+            //
+
+
+            //
+            // end application code here
+            //
+
+            //
+            // disconnect from the finch robot using the finch disconnect method
+            //
+            myFinch.disConnect();
+        }
+    }
+}
